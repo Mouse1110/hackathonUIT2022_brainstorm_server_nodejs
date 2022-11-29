724ed494dd83bbc50c16afba761aead129319350
lamchucuocfuck <101809193+lamchucuocfuck@users.noreply.github.com>
2022-11-29 18:13:23 +0700

    fix heroku server

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.listen(5005, () =>{
-    console.log('Server is running on port 5000.');
-})
+
+app.listen(process.env.PORT || 8080)
 const route = require('./routes');
 const db = require('./config/db');
 
