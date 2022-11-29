bb82f4def4dc037cecd353ea913f3811d8826923
lamchucuocfuck <101809193+lamchucuocfuck@users.noreply.github.com>
2022-11-29 17:43:55 +0700

    update procfile

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -7,11 +7,11 @@ const productController = require('../controllers/ProductController')
 router.get('/show', productController.show);
 // router.get('/', productController.index);
 router.get('/category', productController.category);
-router.get('/user-near', productController.user_near);
-router.post('/', productController.create_product);
-router.post('/buy', productController.buy_product);
-router.get('/buy/list', productController.list_buy_product);
-router.post('/buy-accept', productController.buy_accept);
+// router.get('/user-near', productController.user_near);
+// router.post('/', productController.create_product);
+// router.post('/buy', productController.buy_product);
+// router.get('/buy/list', productController.list_buy_product);
+// router.post('/buy-accept', productController.buy_accept);
 
 
 
