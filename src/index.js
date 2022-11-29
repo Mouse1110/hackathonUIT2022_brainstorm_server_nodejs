2c161e906ca211754913f761d4a270d063af3d50
lamchucuocfuck <101809193+lamchucuocfuck@users.noreply.github.com>
2022-11-29 19:29:53 +0700

    add cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
+ 
+app.use(cors())
+ app.listen(app.listen(process.env.PORT || 8080), function () {
+  console.log('CORS-enabled web server listening')
+})
+    
+    
 
-app.listen(process.env.PORT || 8080)
 const route = require('./routes');
 const db = require('./config/db');
 
